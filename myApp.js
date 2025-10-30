ac7f121ac560910fc63b3419398e0e49ef07b644
unknown <tatian.nico.cachito@gmail.com>
2025-10-30 19:33:48 -0300

    Ocultar header X-Powered-By con helmet.hidePoweredBy()

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
+const helmet = requiere ('helmet');
+const app = express ();
 
-
-
+app.use(helmet.hide.PoweredBy())
+module.exports = app;
 
 
 
