67236c2117bfa6a013de57a077536cfe4dfd6b3a
unknown <tatian.nico.cachito@gmail.com>
2025-11-04 16:20:50 -0300

    Add ieNoOpen for unclear reasons...

--- myApp.js
+++ myApp.js
@@ -6,6 +6,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
 module.exports = app;
 
