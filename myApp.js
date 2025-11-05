9a401c6202eaa2c587899943220fbc18e9e3450c
unknown <tatian.nico.cachito@gmail.com>
2025-11-05 16:46:46 -0300

    add HSTS config

--- myApp.js
+++ myApp.js
@@ -8,6 +8,12 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconds,
+  force: true
+}));
+
 module.exports = app;
 
 
