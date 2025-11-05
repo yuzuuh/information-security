d4a9acf73f3f73a2246dbeb244094130e4e3de0b
yuzuuh <tatian.nico.cachito@gmail.com>
2025-11-05 17:55:39 -0300

    Refactor helmet configuration for security middleware

--- myApp.js
+++ myApp.js
@@ -2,25 +2,25 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
-app.use(helmet.hsts({
-  maxAge: ninetyDaysInSeconds,
-  force: true
-}));
 
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "trusted-cdn.com"]
+app.use(helmet({
+  hidePoweredBy: true,
+  frameguard: { action: 'deny' },
+  xssFilter: true,
+  noSniff: true,
+  ieNoOpen: true,
+  hsts: {
+    maxAge: ninetyDaysInSeconds,
+    force: true
+  },
+  dnsPrefetchControl: false,
+  noCache: true,
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"]
+    }
   }
 }));
 
